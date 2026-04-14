5c514ae2bb3f5db2c012f0e673728824de5fc84a
A Guy called T <taz.khalique@icloud.com>
2026-04-14 23:49:04 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.querySelector('[data-menu-toggle]');
-  const mobileNav = document.querySelector('[data-mobile-nav]');
-  if (toggle && mobileNav) {
-    toggle.addEventListener('click', function () {
-      mobileNav.classList.toggle('open');
-    });
-  }
-});
